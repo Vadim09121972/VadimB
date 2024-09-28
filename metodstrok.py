22d9be9a8ec6e25c233119d98c67dba09ed63929
Vadim09121972 <vbutin1@yandex.ru>
2024-09-28 14:08:18 +0300

    Add files via upload

---
+++ metodstrok.py
@@ -0,0 +1,9 @@
+# name = input("Введите Ваше имя: ")
+# current_year = 2024
+# date_of_birts = int(input("В каком году вы родились? "))
+# age = current_year - date_of_birts
+# print("Здравствуйте,", name)
+# print("В этом году вам", age, "лет")
+# print('привет, я строка в нижнем регистре')
+# print('привет, я строка в нижнем регистре'.upper().lower())
+print('привет, я строка в нижнем регистре'.replace('привет', 'пока'))
