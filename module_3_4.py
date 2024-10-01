239bff6c0ff208e969ba048de428d298498c6ea8
Vadim09121972 <vbutin1@yandex.ru>
2024-10-01 16:23:27 +0300

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,10 @@
+def single_root_words(*other_words, root_words = 'хлеб'):
+    same_words = []                       
+    for i in other_words:
+        if root_words.lower() in i.lower():
+            same_words.append(i)            
+
+    return print(same_words)
+
+rezalt_1 = single_root_words( 'хлебный', 'нахлебник', 'хлев', 'похлёбка', 'хлебало')
+rezalt_2 = single_root_words('небосклон', 'поднебесье', 'недоросль', 'непогода', 'небожитель', root_words = 'небо')
